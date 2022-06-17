f14174958831adaf5c32c31a353ba4965d020d1f
Mayel de Borniol <git@mayel.space>
2022-06-17 15:08:16 +1200

    Fix url

--- lib/blurred.ex
+++ lib/blurred.ex
@@ -3,34 +3,32 @@ defmodule Bonfire.Files.Blurred do
 
   def blurred(definition \\ nil, media_or_path)
   def blurred(definition, %{path: path} = _media), do: blurred(definition, path)
-  def blurred(_definition, path) when is_binary(path) do
+  def blurred(_definition, original_path) when is_binary(original_path) do
 
-    path = String.trim_leading(path, "/")
-    final_path = path<>"_preview.jpg"
+    path = String.trim_leading(original_path, "/")
+    blurred_path = path<>"_preview.jpg"
 
-    ret_path = if String.starts_with?(path, "http") or is_nil(path) or path =="" or not File.exists?(path) or System.get_env("CI") do
+    if String.starts_with?(path, "http") or is_nil(path) or path =="" or not File.exists?(path) or System.get_env("CI") do
       debug(path, "it's an external or invalid image, skip")
-      path
+      original_path
     else
-      if File.exists?(final_path) do
-        debug(final_path, "blurred jpeg already exists :)")
-        final_path
+      if File.exists?(blurred_path) do
+        debug(blurred_path, "blurred jpeg already exists :)")
+        "/#{blurred_path}"
       else
-        debug(final_path, "first time trying to get this blurred jpeg?")
+        debug(blurred_path, "first time trying to get this blurred jpeg?")
 
-        with saved_path when is_binary(saved_path) <- Bonfire.Files.Image.Edit.blur(path, final_path) do
+        with saved_path when is_binary(saved_path) <- Bonfire.Files.Image.Edit.blur(path, blurred_path) do
 
             debug(saved_path, "saved blurred jpeg")
 
-            saved_path
+            "/#{saved_path}"
           else e ->
             error(e)
-            path
+            original_path
         end
       end
     end
-
-    "/#{ret_path}"
   end
 
 
