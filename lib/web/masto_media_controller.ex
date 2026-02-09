5864d3b18c3b04ca43a8c9ca0a5c93b870eef89d
ivan <ivanminutillo@outlook.it>
2026-02-09 15:53:24 +0100

    https://github.com/bonfire-networks/bonfire-app/issues/916

--- lib/web/masto_media_controller.ex
+++ lib/web/masto_media_controller.ex
@@ -3,28 +3,12 @@ if Application.compile_env(:bonfire_api_graphql, :modularity) != :disabled do
     @moduledoc "Mastodon-compatible Media REST endpoints."
 
     use Bonfire.UI.Common.Web, :controller
-    import Untangle
 
     alias Bonfire.Files.API.GraphQLMasto.Adapter
 
-    def show(conn, %{"id" => id} = params) do
-      debug(params, "GET /api/v1/media/#{id}")
-      Adapter.get_media(id, conn)
-    end
-
-    def update(conn, %{"id" => id} = params) do
-      debug(params, "PUT /api/v1/media/#{id}")
-      Adapter.update_media(id, params, conn)
-    end
-
-    def create(conn, params) do
-      debug(params, "POST /api/v1/media")
-      Adapter.upload_media(params, conn)
-    end
-
-    def create_v2(conn, params) do
-      debug(params, "POST /api/v2/media")
-      Adapter.upload_media(params, conn)
-    end
+    def show(conn, %{"id" => id}), do: Adapter.get_media(id, conn)
+    def update(conn, %{"id" => id} = params), do: Adapter.update_media(id, params, conn)
+    def create(conn, params), do: Adapter.upload_media(params, conn)
+    def create_v2(conn, params), do: Adapter.upload_media(params, conn)
   end
 end
